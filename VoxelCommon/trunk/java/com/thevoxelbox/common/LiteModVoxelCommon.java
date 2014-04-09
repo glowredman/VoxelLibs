c4a00f66d90dba7b606f74fd75a671359ecf54aa
mumfrey <mumfrey@40d87847-b7ac-4e2e-a40d-cabf05508630>
2014-04-09 10:20:02 +0000

    VoxelCommon - make ChatMessageBuilder more like StringBuilder and unify src formatting, version bump to 2.2.5

--- VoxelCommon/trunk/java/com/thevoxelbox/common/LiteModVoxelCommon.java
+++ VoxelCommon/trunk/java/com/thevoxelbox/common/LiteModVoxelCommon.java
@@ -33,7 +33,7 @@ public class LiteModVoxelCommon implements LiteMod
 	/**
 	 * 
 	 */
-	public static final String VERSION = "2.2.4";
+	public static final String VERSION = "2.2.5";
 
 	public static final ResourceLocation GUIPARTS = new ResourceLocation("voxelcommon", "textures/gui/guiparts.png");
 
